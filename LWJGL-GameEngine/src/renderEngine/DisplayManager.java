f9a02ab7d494476afc647ff4f7e0946f7407503c
Victor Betini <betinivictor@gmail.com>
2019-08-29 00:51:09 -0300

    Created DisplayManager and the MainGameLoop classes

---
+++ LWJGL-GameEngine/src/renderEngine/DisplayManager.java
@@ -0,0 +1,42 @@
+package renderEngine;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
+
+public class DisplayManager {
+	
+	private static final int W = 1280;
+	private static final int H = 720;
+	private static final int FPS = 720;
+	
+	public static void createDisplay() {
+		
+		ContextAttribs attribs = new ContextAttribs(3,2);
+		attribs.withForwardCompatible(true);
+		attribs.withProfileCore(true);
+		
+		try {
+			Display.setDisplayMode(new DisplayMode(W, H));
+			Display.create(new PixelFormat(), attribs);
+			Display.setTitle("LWJGL - Game");
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		GL11.glViewport(0, 0, W, H);
+	}
+	
+	public static void updateDisplay() {
+		Display.sync(FPS);
+		Display.update();
+	}
+	
+	public static void closeDisplay() {
+		Display.destroy();
+	}
+
+}
